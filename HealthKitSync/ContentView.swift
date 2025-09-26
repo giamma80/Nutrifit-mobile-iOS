7a583cadd47bbef38536cd52cd1fa72bd8ae5f48
Gianmarco Morelli <gianmarco.morelli@alten.it>
2025-09-26 11:28:36 +0200

    Initial Commit

---
+++ HealthKitSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HealthKitSync
+//
+//  Created by Gianmarco Morelli on 26/09/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HealthKitSync/HealthKitSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitSyncApp.swift
+//  HealthKitSync
+//
+//  Created by Gianmarco Morelli on 26/09/25.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
